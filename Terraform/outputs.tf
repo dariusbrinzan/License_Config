fb0f4bb831ec26d418f3e1a9239bc549040775c7
dariusbrinzan <brinzandarius27@gmail.com>
2024-04-02 11:59:34 +0300

    config

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ids" {
-  value = aws_instance.example[*].id
+  value = aws_instance.ec2[*].id
 }
 
 output "instance_public_ips" {
-  value = aws_instance.example[*].public_ip
+  value = aws_instance.ec2[*].public_ip
 }
