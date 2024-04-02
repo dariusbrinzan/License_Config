eac1a2befd3855bc26b74b3e33fbf158c27b4f8b
dariusbrinzan <brinzandarius27@gmail.com>
2024-04-02 19:12:54 +0300

    sg

--- Terraform/security_groups.tf
+++ Terraform/security_groups.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
