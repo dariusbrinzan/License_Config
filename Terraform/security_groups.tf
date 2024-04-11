9f676026230347cc940072348299f364f29c31c1
dariusbrinzan <brinzandarius27@gmail.com>
2024-04-11 14:22:18 +0300

    Modified sg rules

--- Terraform/security_groups.tf
+++ Terraform/security_groups.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+  description = "Allow SSH, Next.js applications and NGINX inbound traffic"
 
   ingress {
     from_port   = 22
@@ -12,6 +12,13 @@ resource "aws_security_group" "allow_ssh" {
   ingress {
     from_port   = 3000
     to_port     = 3004
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
